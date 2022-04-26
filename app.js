7295d48023d1799e5a111d792339ac4031459169
agahtugrulhan <tugrulpol@gmail.com>
2022-04-26 23:46:51 +0300

    my commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const closedFaced = document.querySelector('.closed')
+const openFaced = document.querySelector('.open')
+
+//ADD EVENT LISTENER
+
+closedFaced.addEventListener("click", () => {
+    if(openFaced.classList.contains('open')) {
+        openFaced.classList.add('active');
+        closedFaced.classList.remove('active');   
+    }
+});
+
+openFaced.addEventListener("click", () => {
+    if(closedFaced.classList.contains('closed')) {
+        closedFaced.classList.add('active');
+        openFaced.classList.remove('active');   
+    }
+});
+
